6f4f2ea2aeca03a4535e2452eb9718f17436fc8c
Felix Hillingshaeuser <felix@mxcore.de>
2018-06-19 13:51:11 +0200

    Fix typos and run go fmt
    fix two typos in the comments and run go fmt which fixed formating in handler.go

--- handler.go
+++ handler.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"net/http"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
 )
 
 // handlerFunc can be used as handler for http.HandleFunc()
